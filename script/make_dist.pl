c272b03d0b8b56dd48025278c2bc7ba13c391900
Rutger Vos <rutgeraldo@gmail.com>
2017-01-17 15:04:28 +0100

    now makes ultrametric and reconstitutes tree directly from MRP

--- script/make_dist.pl
+++ script/make_dist.pl
@@ -3,25 +3,27 @@ use strict;
 use warnings;
 use Getopt::Long;
 use Data::Dumper;
-use Bio::Align::DNAStatistics;
-use Bio::Tree::DistanceFactory;
 use Bio::Phylo::Factory;
 use Bio::Phylo::Forest::Tree;
 use Bio::Phylo::Util::Logger ':levels';
 use Bio::Phylo::IO qw'parse_tree unparse';
 
+# The name of this script is a misnomer. In fact it re-assembles trees from MRP matrices.
+# the input is a matrix of the form:
+# BLOCK_ID <TAB> TAXON_ID <TAB> CHARACTERS
+
 # process command line arguments
 my $infile;
 my $verbosity = WARN;
+my $equal;
 GetOptions(
 	'infile=s' => \$infile,
 	'verbose+' => \$verbosity,
+	'equal'    => \$equal,
 );
 
 # instantiate helper objects
 my $fac = Bio::Phylo::Factory->new;
-my $dna = Bio::Align::DNAStatistics->new;
-my $njf = Bio::Tree::DistanceFactory->new( '-method' => "NJ" );
 my $log = Bio::Phylo::Util::Logger->new(
 	'-level' => $verbosity,
 	'-class' => 'main',
@@ -100,27 +102,46 @@ sub analyze_matrix {
 	my ( $start, $end, %data ) = @_;
 	
 	# populate matrix
-	my $m = $fac->create_matrix( '-type' => 'dna' );
+	my %matrix;
+	my $nchar = ( $end - $start );
 	for my $row ( keys %data ) {
 		my $char = substr $data{$row}, $start, ( $end - $start );
-		$char =~ tr/012/AC?/;
-		$m->insert(
-			$fac->create_datum(
-				'-type' => 'dna',
-				'-name' => $row,
-				'-char' => $char,
-			)
-		);
-		$log->debug("added $row => $char");
-	}
+		$matrix{$row} = [ split //, $char ];
+	}	
 	
-	# do NJ tree
-	my $dist = $dna->distance(
-		'-align'  => $m,
-		'-method' => 'Uncorrected',
-	);
-	my $tree = Bio::Phylo::Forest::Tree->new_from_bioperl($njf->make_tree($dist));
-	$log->info($tree->to_newick);
+	# sort columns
+	my @indices;
+	for my $i ( 0 .. $nchar - 1 ) {
+		my $total = 0;
+		$total += $matrix{$_}->[$i] for keys %matrix;
+		push @indices, [ $i, $total ];
+	}
+	my @sorted = sort { $a->[1] <=> $b->[1] } @indices;
+	
+	# start building the tree
+	my $tree = $fac->create_tree;
+	my %tips = map { $_ => $fac->create_node( '-name' => $_ ) } keys %matrix;
+	my $root = $fac->create_node;
+	$_->set_parent($root) for values %tips;
+	$tree->insert($root);
+	$tree->insert($_) for values %tips;
+	COL: for my $col ( @sorted ) {
+		my @tips  = grep { $matrix{$_}->[ $col->[0] ] } keys %matrix; # labels
+		my @nodes = @tips{@tips}; # node objects
+		my %uniq  = map { $_->get_id => $_ } @nodes;
+		@nodes = values %uniq;
+		next COL if scalar(@nodes) < 2;
+		
+		# instantiate new parent
+		my $parent = $fac->create_node;
+		$_->set_parent($parent) for @nodes;
+		$parent->set_parent($root);
+		$tree->insert($parent);
+		$tips{$_} = $parent for @tips;
+	}
+	$tree->remove_unbranched_internals;
+	my @nameless = grep { $_->get_name eq '' } @{ $tree->get_terminals };
+	$tree->prune_tips(\@nameless);
 	make_tree_output($tree);
 }
 
@@ -139,6 +160,41 @@ sub make_tree_output {
 			$n->set_branch_length(1);
 		}
 	});
+	
+	# make ultrametric instead of equal lengths
+	if ( not $equal ) {
+		my $tallest = 0;
+		for my $tip ( @{ $tree->get_terminals } ) {
+			my $anc  = $tip->get_ancestors;
+			my $len  = scalar @$anc;
+			$tallest = $len if $len > $tallest;			
+			$tip->set_generic( 'heights' => [ $len ], 'added' => 0 );
+			for my $n ( @$anc ) {
+				my $heights = $n->get_generic('heights') || [];
+				push @$heights, $len;
+				$n->set_generic( 'heights' => $heights, 'added' => 0 );
+			}
+		}
+		$tree->visit_depth_first(
+			'-pre' => sub {
+				my $node = shift;
+				if ( not $node->is_root ) {
+					my $added = 0;
+					if ( my $p = $node->get_parent ) {
+						$added = $p->get_generic('added');
+					}
+					my ($tall) = sort { $b <=> $a } @{ $node->get_generic('heights') };
+					my $diff = ( $tallest - $tall ) - $added;
+					if ( $diff > 0 ) {
+						my $newl = $node->get_branch_length + $diff;
+						$node->set_branch_length($newl);
+						$node->set_generic( 'added' => $added + $diff );
+					}
+				}
+			}
+		);
+	}
+	
 	print $tree->to_newick, "\n";
 }
 
